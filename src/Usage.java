f3d85b9435d9bf8cb19fd5fcc1b5e1978ee03299
chinge55 <sangamkhnl@gmail.com>
2019-02-21 12:18:49 -0500

    Hope everything workd

--- src/Usage.java
+++ src/Usage.java
@@ -4,7 +4,7 @@ public class Usage
 	public Usage()
 	{
 		System.out.println("USAGE: java main [option] input_file");
-		System.out.println("Options:")
+		System.out.println("Options:");
 		System.out.println("1. -Phone: Searches For Phone numbers(Nepal Only)");
 		System.out.println("2. -String: Searches For a String , String has to be entered again");
 		System.out.println("3. -Email: Searches For emails");
